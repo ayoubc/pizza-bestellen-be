068dff6fac3446de7a0735289e273e9bd4aa548e
ayoubc <ayoubch807@gmail.com>
2020-07-02 04:20:19 +0100

    pizzas controller(router)

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const connection = require('./connection');
-
+const bodyParser = require('body-parser');
+const pizzaRouter = require('./routers/pizza-router')(connection);
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 
-
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api', pizzaRouter);
 
 app.get('/', (req, res) => {
-    connection.query('SELECT * FROM pizza', (err, rows, fields) => {
-        if(!!err) {
-            console.log(err);
-        }
-        else{
-            res.status(200).send({data: rows});
-        }
-    });
-    // return res.json({ text: "Hello World" });
+    return res.json({ version: "1.0.0" });
 });
 
 app.server = app.listen(PORT, (req, res) => {
